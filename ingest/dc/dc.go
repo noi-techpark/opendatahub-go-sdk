3208d5638a13cca24af50acdc0ae6794a3a4c026
Clemens Zagler <c.zagler@noi.bz.it>
2026-07-03 11:07:03 +0200

    feat(dc): default RAW_WRITER_URL to raw-writer-2 cluster endpoint

--- ingest/dc/dc.go
+++ ingest/dc/dc.go
@@ -20,7 +20,7 @@ import (
 
 type Env struct {
 	PROVIDER       string
-	RAW_WRITER_URL string
+	RAW_WRITER_URL string `default:"http://raw-writer-2.core.svc.cluster.local"`
 	MQ_URI         string
 	MQ_EXCHANGE    string `default:"ingress"`
 	MQ_CLIENT      string
